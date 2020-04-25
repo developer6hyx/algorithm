1921caff8236652b703dbc986810e75d5f4e5b65
developer6hyx <62423717+developer6hyx@users.noreply.github.com>
2020-04-25 11:32:25 +0800

    Create quicksort_cpp.cpp

---
+++ quicksort_cpp.cpp
@@ -0,0 +1,34 @@
+int a[10001] = {};
+void quicksort(int left, int right)
+{
+	if(left > right)
+	{
+		return ;
+	}
+	int i, j, t, temp;
+	temp = a[left];
+	i = left;
+	j = right;
+	while(i != j)
+	{
+		while(a[j] >= temp && i < j)
+		{
+			j--;
+		}
+		while(a[i] <= temp && i < j)
+		{
+			i++;
+		}
+		if(i < j)
+		{
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+	}
+	a[left] = a[i];
+	a[i] = temp;
+	
+	quicksort(left, i - 1);
+	quicksort(i + 1, right);
+}
